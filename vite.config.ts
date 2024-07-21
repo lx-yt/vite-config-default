ff9601b13482947373de6cb69c9b68cf9dbb4340
Lexyth <marfy@web.de>
2024-07-21 03:17:48 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+/// <reference types="vitest/config" />
+
+import eslint from "vite-plugin-eslint2";
+import checker from "vite-plugin-checker";
+import dts from "vite-plugin-dts";
+
+import type { UserConfig } from "vite";
+
+const baseConfig: UserConfig = {
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+    lib: {
+      entry: "src/index.ts",
+      name: "Vite Config",
+      fileName: "index",
+      formats: ["es"],
+    },
+  },
+  plugins: [
+    eslint({
+      cache: false,
+      emitErrorAsWarning: true,
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+      exclude: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+    }),
+    checker({ typescript: true }),
+    dts({ rollupTypes: true }),
+  ],
+  server: {
+    open: true,
+    port: 3000,
+  },
+  test: {
+    environment: "jsdom",
+  },
+};
+
+export default baseConfig;
